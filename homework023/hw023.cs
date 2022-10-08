ebb2be51252b6ff5746270c0827ee366c7281b6a
Pavel <pashok_163@mail.ru>
2022-10-08 22:05:49 +0300

    Добавили папку с решением 3 задачи 3 семинара

---
+++ homework023/hw023.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+void tableN()
+{
+Console.WriteLine("Введите число (N): ");
+int N = int.Parse(Console.ReadLine());
+int[] myArr = new int[N];
+int numb=1;
+for (int i=0;i< N;i++)
+{
+    myArr[i] = Convert.ToInt32(Math.Pow(numb,3));
+    numb++;
+}
+Console.WriteLine("Tаблица кубов чисел от 1 до N: "+ String.Join(", ",myArr));
+}
+tableN();
+ 
