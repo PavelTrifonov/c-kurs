d11f5ea3da255743320e6d856531b5ec36ceb1c4
Pavel <pashok_163@mail.ru>
2022-10-09 14:30:27 +0300

    изменили файл тест1

--- test001/test001.cs
+++ test001/test001.cs
@@ -1,8 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string arr = Console.ReadLine();
-string[] arr2=arr.Split(" ");
-Console.Write(String.Join(",", arr2));
-int r=arr.Length-1;
-for(int i=0; i<arr2.Length;i++)
-   if(arr2[r-i]!=20) 
-   Console.WriteLine(arr2[3]);
+// string arr = Console.ReadLine();
+// string[] arr2=arr.Split(" ");
+// Console.Write(String.Join(",", arr2));
+// int r=arr.Length-1;
+// for(int i=0; i<arr2.Length;i++)
+//    if(arr2[r-i]!=20) 
+//    Console.WriteLine(arr2[3]);
+   //List<int> myNewArrey()
+// {
+// List<int> myList= new List<int>();
+// System.Console.WriteLine("Введите элементы списка: ");
+// string el="" ;
+// while(el!="end")
+// {el = (Console.ReadLine());
+// myList.Add(int.Parse(el));
+// }
+// Console.WriteLine(string.Join(", ", myList));
+// }
+// //myNewArrey();
