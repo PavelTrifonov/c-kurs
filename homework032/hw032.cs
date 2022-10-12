6b3348c0649c9efe9d06fdb4084007ef80e08f12
Pavel <pashok_163@mail.ru>
2022-10-13 00:08:48 +0300

    Изменили код задачи

--- homework032/hw032.cs
+++ homework032/hw032.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход число
+ и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+void sumnum()
+{
+System.Console.WriteLine("Введите число : ");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+while(A!=0)
+{ sum= sum + A%10;
+    A = A/10;}
+ System.Console.WriteLine("Сумма цифр в числе: "+sum);
+}
+sumnum();
