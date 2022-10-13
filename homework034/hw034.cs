758d130877f72cccc8b21cb92189254e4719a02d
Pavel <pashok_163@mail.ru>
2022-10-13 19:32:50 +0300

    Добавили решение четвертой задачи 4 семинара

---
+++ homework034/hw034.cs
@@ -0,0 +1,23 @@
+﻿// Преобразовать массив так, чтобы сначала шли 
+//нулевые элементы, а затем все остальные.
+//-1 -3 -1 -3 -1 0 -4 -2 -> 0 -3 -1 -3 -1 -1 -4 -2
+int col = new Random().Next(5,10);
+int[] myArr = new int[col];
+for (int i=0;i< col;i++)
+{
+    myArr[i] = new Random().Next(-3,1);
+}
+Console.WriteLine("Старый массив:: "+ String.Join(", ",myArr));
+int j=0;
+for(int i = 0;i<col;i++)
+{
+int c=0;
+if (myArr[i]==0)
+{
+c=myArr[j];
+myArr[j]=myArr[i];
+myArr[i]=c;
+j++;
+}
+}
+Console.WriteLine("Новый массив: "+ String.Join(", ",myArr));
