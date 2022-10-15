19a90f9269cc664aa6e35a494faefebcb2f778a4
Pavel <pashok_163@mail.ru>
2022-10-15 23:05:18 +0300

    Добавили папку с решениями 5 семинара и папку с решением задачи 34

---
+++ homework041/hw041.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 34: Задайте массив заполненный случайными 
+положительными трёхзначными числами. Напишите 
+программу, которая покажет количество чётных 
+чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+void chetNumInList()
+{
+List<int> myList=new List<int>();
+System.Console.WriteLine("Задайте размер массива: ");
+int num=int.Parse(Console.ReadLine());
+for(int i =0;i<num;i++)
+myList.Add(new Random().Next(100,999));
+System.Console.WriteLine(String.Join(" ",myList));
+int col=0;
+foreach(int i in myList)
+if (i%2==0)
+col++;
+System.Console.WriteLine("Kоличество чётных чисел в массиве: "+col);
+}
+chetNumInList();
