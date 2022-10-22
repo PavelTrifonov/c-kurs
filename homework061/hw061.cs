c3239266005f2bc1f03edb2c7ec2117cbe1b500e
Pavel <pashok_163@mail.ru>
2022-10-22 15:27:47 +0300

    Добавили папку с решением 47 задачи из7  семинара

---
+++ homework061/hw061.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+List<List<double>> metodArray()
+{
+    List<List<double>> array = new List<List<double>>();
+    System.Console.WriteLine("Введите число строк: ");
+    int lines = int.Parse(Console.ReadLine());
+    System.Console.WriteLine("Введите число столбцов: ");
+    int columns = int.Parse(Console.ReadLine());
+    for (int i = 0; i < lines; i++)
+    {
+        array.Add(new List<double>());
+        for (int j = 0; j < columns; j++)
+        {
+            array[i].Add(Math.Round(new Random().NextDouble() * 10, 2));
+            //System.Console.Write(array[i][j] + "\t");
+        }
+        //System.Console.WriteLine();
+    }
+    return array;
+}
+void printArray ()
+{
+    List<List<double>> arr = metodArray();
+    for (int i = 0; i < arr.Count; i++)
+    {
+        for (int j = 0; j < arr[i].Count; j++)
+        {
+            System.Console.Write(arr[i][j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+printArray();
---
+++ semm07/semm07.cs
@@ -0,0 +1,50 @@
+﻿/*Условие
+Дан двумерный массив и два числа: i и j. Поменяйте в массиве 
+столбцы с номерами i и j и выведите результат.
+
+Программа получает на вход размеры массива n и m, затем 
+элементы массива, затем числа i и j.
+
+Решение оформите в виде функции swap_columns(a, i, j)*/
+int[,] RandMass()
+{
+System.Console.WriteLine("Введите число строк массива: ");
+int n=int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число столбцов массива: ");
+int m=int.Parse(Console.ReadLine());
+int[,] mass=new int[n,m];
+for(int i =0;i<mass.GetLength(0);i++)
+for(int j =0;j<mass.GetLength(1);j++)
+mass[i,j]=new Random().Next(0,10);
+return mass;
+}
+int[,] SwapColumns(int[,] NewMass,int a,int b)
+{
+    int c=0;
+for(int i =0;i<NewMass.GetLength(0);i++)
+{
+for(int j =0;j<NewMass.GetLength(1);j++)
+if(j==a)
+{
+c=NewMass[i,a];
+NewMass[i,a]=NewMass[i,b];
+NewMass[i,b]=c;
+}
+}
+return NewMass;
+}
+int[,] a=RandMass();
+SwapColumns(a,1,4);
+void print(int [,] mass)
+{
+for (int i = 0; i <mass.GetLength(0); i++)
+    {
+        for (int j = 0; j <mass.GetLength(1); j++)
+            System.Console.Write(mass[i,j] + " ");
+        System.Console.WriteLine("");
+
+    } 
+}
+print(a);
+System.Console.WriteLine("");
+print(SwapColumns(a,1,4));
