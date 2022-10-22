672ad8fe0673d4479f6a6326becb94e341479080
Pavel <pashok_163@mail.ru>
2022-10-22 15:29:55 +0300

    Изменили код в папке hw053

--- homework053/hw053.cs
+++ homework053/hw053.cs
@@ -5,16 +5,16 @@ y = k1 * x + b1, y = k2 * x + b2;
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
 List<string> KoordX(float b1, float k1, float b2, float k2)
 {
-    string y = Convert.ToString((b1*k2-b2*k1)/(k2-k1));
-    string  x = Convert.ToString((b2-b1)/(k1-k2));
-    List<string> X= new List<string>();
+    string y = Convert.ToString((b1 * k2 - b2 * k1) / (k2 - k1));
+    string x = Convert.ToString((b2 - b1) / (k1 - k2));
+    List<string> X = new List<string>();
     X.Add(x);
     X.Add(y);
-return X;
+    return X;
 }
 void print()
- {
-     List<string> XY= new List<string>(KoordX(2,5,4,9));
-     System.Console.WriteLine("Координаты пересечения прямых: ("+String.Join("; ",XY)+")");
- }
- print();
+{
+    List<string> XY = new List<string>(KoordX(2, 5, 4, 9));
+    System.Console.WriteLine("Координаты пересечения прямых: (" + String.Join("; ", XY) + ")");
+}
+print();
