f812a2054395b7183a14e467a5590f6321d3e049
Pavel <pashok_163@mail.ru>
2022-11-12 19:16:42 +0300

    Добавили папку с решением задачи 64 из 9 семинара

---
+++ homework091/hw091.cs
@@ -0,0 +1,12 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+string CountDown(int N)
+{
+    if(N==1) return "1";
+    else return ($"{N}, {CountDown(N-1)}");
+}
+System.Console.WriteLine("Введите число");
+System.Console.WriteLine(CountDown(int.Parse(Console.ReadLine())));
