ccda0aa99ee874179bd0246bf3d5ce98b2c3e64d
Pavel <pashok_163@mail.ru>
2022-11-12 19:42:44 +0300

    Добавили папку с решением задачи 66 из 9 семинара

---
+++ homework092/hw092.cs
@@ -0,0 +1,11 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int sumNumb(int M, int N)
+{
+    if (M==N) return M;
+    else return (M+sumNumb(M+1,N));
+}
+System.Console.WriteLine(sumNumb(1,15));
